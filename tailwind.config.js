bee5c6bd6e2f468be3346875f5f26cf32429b7aa
Joshua <joshjoshuap1@gmail.com>
2022-09-09 16:07:07 +0800

    chore: add font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Rubik", "sans-serif"],
+      serif: ["Roboto Slob", "serif"],
+    },
   },
   plugins: [],
-}
+};
