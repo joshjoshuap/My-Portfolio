f5498de65d16ebe5313a680b71f0a8fb6703a0e6
Joshua <joshjoshuap1@gmail.com>
2022-09-10 16:09:11 +0800

    style: change heading title color, text center

--- src/pages/sections/Header.js
+++ src/pages/sections/Header.js
@@ -1,9 +1,10 @@
 const Header = () => {
   return (
-    <header className="w-11/12 py-14 mx-auto">
-      <div className="w-fit">
-        <h1 className="text-neutral-600 text-7xl">Joshua</h1>
-        <h1 className="text-blue-600 text-7xl"> Pautanes </h1>
+    <header className="w-10/12 py-14 mx-auto">
+      <div className="text-center">
+        <h1 className="text-neutral-600 text-7xl">
+          Joshua <span className="text-blue-600 ">Pautanes</span>
+        </h1>
         <h3 className="text-2xl"> Fresh Grad </h3>
       </div>
     </header>
