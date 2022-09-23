ebd40ebb3c31355c7690a9995acf47c9384719a5
Joshua <joshjoshuap1@gmail.com>
2022-09-23 18:55:13 +0800

    chore: redesign footer

--- src/pages/sections/Footer.js
+++ src/pages/sections/Footer.js
@@ -1,7 +1,14 @@
 const Footer = () => {
+  let year = new Date().getFullYear();
+
   return (
-    <footer className="bg-neutral-900 p-3">
-      <h2 className="text-neutral-100 text-center">@ Copyright 2022 : Joshua Pautanes</h2>
+    <footer className="flex justify-center gap-10 bg-neutral-900 text-neutral-50 text-xl p-5">
+      <p>&copy; Copyright {year} </p>
+      <p>
+        <a href="https://joshjoshuap.vercel.app/" className="border-b-4 border-transparent hover:border-blue-600 transition-all duration-300 ease-in-out" target="_blank" rel='noreferrer'>
+          Build By: Joshua Pautanes
+        </a>
+      </p>
     </footer>
   );
 };
