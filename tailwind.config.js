baf975f5eda4771f5b98394cd6862ea98c7c5d29
Joshua <joshjoshuap1@gmail.com>
2023-08-27 20:15:48 +0800

    chore: add & configure NextUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+import { nextui } from '@nextui-org/react';
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#040D12',
+        gray: '#454545',
+        white: '#EFF5F5',
+        darkblue: '#27374D',
+        blue: '#164B60',
+        lightblue: '#3A98B9',
+        darkorange: '#F97B22',
+        orange: '#F29727',
+        lightorange: '#FFB84C',
+        darkgreen: '#379237',
+        green: '#54B435',
+        lightgreen: '#82CD47',
+      },
+    },
     fontFamily: {
       sans: ['Poppins', 'sans-serif'],
       serif: ['Roboto Slob', 'serif'],
     },
-    colors: {
-      black: '#040D12',
-      gray: '#454545',
-      white: '#EFF5F5',
-      darkblue: '#27374D',
-      blue: '#164B60',
-      lightblue: '#3A98B9',
-      darkorange: '#F97B22',
-      orange: '#F29727',
-      lightorange: '#FFB84C',
-      darkgreen: '#379237',
-      green: '#54B435',
-      lightgreen: '#82CD47',
-    },
   },
-  plugins: [],
+  darkMode: 'class',
+  plugins: [
+    nextui({
+      addCommonColors: true,
+    }),
+  ],
 };
