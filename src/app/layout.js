6a74546389d7de29054123a0ca7cd790445d8128
Joshua <joshjoshuap1@gmail.com>
2023-08-28 15:05:55 +0800

    chore: add scroll smooth

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <head>
         <link rel="icon" href="/favicon.ico" />
 
