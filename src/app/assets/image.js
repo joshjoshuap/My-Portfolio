d62518984023ede4abfdda2fa603e3dc626ca272
Joshua <joshjoshuap1@gmail.com>
2023-08-29 19:39:49 +0800

    chore: change image extesion png to jpg

--- src/app/assets/image.js
+++ src/app/assets/image.js
@@ -1,25 +1,25 @@
-import design1 from '/public/image/design-1.png';
-import design2 from '/public/image/design-2.png';
-import design3 from '/public/image/design-3.png';
-import design4 from '/public/image/design-4.png';
-import design5 from '/public/image/design-5.png';
-import design6 from '/public/image/design-6.png';
-import design7 from '/public/image/design-7.png';
-import design8 from '/public/image/design-8.png';
-import design9 from '/public/image/design-9.png';
-import design10 from '/public/image/design-10.png';
-import design11 from '/public/image/design-11.png';
-import design12 from '/public/image/design-12.png';
-import design13 from '/public/image/design-13.png';
-import design14 from '/public/image/design-14.png';
-import design15 from '/public/image/design-15.png';
-import design16 from '/public/image/design-16.png';
+import design1 from '/public/image/design-1.jpg';
+import design2 from '/public/image/design-2.jpg';
+import design3 from '/public/image/design-3.jpg';
+import design4 from '/public/image/design-4.jpg';
+import design5 from '/public/image/design-5.jpg';
+import design6 from '/public/image/design-6.jpg';
+import design7 from '/public/image/design-7.jpg';
+import design8 from '/public/image/design-8.jpg';
+import design9 from '/public/image/design-9.jpg';
+import design10 from '/public/image/design-10.jpg';
+import design11 from '/public/image/design-11.jpg';
+import design12 from '/public/image/design-12.jpg';
+import design13 from '/public/image/design-13.jpg';
+import design14 from '/public/image/design-14.jpg';
+import design15 from '/public/image/design-15.jpg';
+import design16 from '/public/image/design-16.jpg';
 
 import hackerImage from '/public/image/hacker.png';
 import headerImage from '/public/image/header-image.png';
-import webdevresourcesImage from '/public/image/webdeveresources-image.png';
-import insightmarketImage from '/public/image/insighmarket-image.png';
-import postpostImage from '/public/image/postpost-image.png';
+import webdevresourcesImage from '/public/image/webdeveresources-image.jpg';
+import insightmarketImage from '/public/image/insighmarket-image.jpg';
+import postpostImage from '/public/image/postpost-image.jpg';
 
 export {
   design1,
