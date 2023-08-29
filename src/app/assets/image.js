6650138277b43a07e3fa47d5f5e39a112b677176
Joshua <joshjoshuap1@gmail.com>
2023-08-29 19:07:01 +0800

    chore: import images

---
+++ src/app/assets/image.js
@@ -0,0 +1,46 @@
+import design1 from '/public/image/design-1.png';
+import design2 from '/public/image/design-2.png';
+import design3 from '/public/image/design-3.png';
+import design4 from '/public/image/design-4.png';
+import design5 from '/public/image/design-5.png';
+import design6 from '/public/image/design-6.png';
+import design7 from '/public/image/design-7.png';
+import design8 from '/public/image/design-8.png';
+import design9 from '/public/image/design-9.png';
+import design10 from '/public/image/design-10.png';
+import design11 from '/public/image/design-11.png';
+import design12 from '/public/image/design-12.png';
+import design13 from '/public/image/design-13.png';
+import design14 from '/public/image/design-14.png';
+import design15 from '/public/image/design-15.png';
+import design16 from '/public/image/design-16.png';
+
+import hackerImage from '/public/image/hacker.png';
+import headerImage from '/public/image/header-image.png';
+import webdevresourcesImage from '/public/image/webdeveresources-image.png';
+import insightmarketImage from '/public/image/insighmarket-image.png';
+import postpostImage from '/public/image/postpost-image.png';
+
+export {
+  design1,
+  design2,
+  design3,
+  design4,
+  design5,
+  design6,
+  design7,
+  design8,
+  design9,
+  design10,
+  design11,
+  design12,
+  design13,
+  design14,
+  design15,
+  design16,
+  hackerImage,
+  headerImage,
+  insightmarketImage,
+  postpostImage,
+  webdevresourcesImage,
+};
