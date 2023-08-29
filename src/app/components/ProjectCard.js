ff1f3a1d9f556229049b8d9c29ef40d6358483b8
Joshua <joshjoshuap1@gmail.com>
2023-08-29 20:39:40 +0800

    fix: fix button length

--- src/app/components/ProjectCard.js
+++ src/app/components/ProjectCard.js
@@ -40,15 +40,15 @@ export default function ProjectCard({ title, desc, image, github, website, techn
               width={60}
             />
           </div>
-          <div className="flex justify-around mt-3">
+          <div className="flex justify-center gap-2 mt-3">
             <Link href={github} target="_blank">
-              <div className="bg-white flex justify-center items-center gap-2 py-2 w-40 px-3 border-2 border-transparent rounded-lg hover:bg-gray transition-all duration-200">
+              <div className="bg-white flex justify-center items-center gap-2 py-2 w-fit px-3 border-2 border-transparent rounded-lg hover:bg-gray transition-all duration-200">
                 <Image src={githubIcon} height={40} width={40} />
                 <p className="text-xl font-medium">GITHUB</p>
               </div>
             </Link>
             <Link href={website} target="_blank">
-              <div className="bg-white flex justify-center items-center gap-2 py-2 w-40 px-3 border-2 border-transparent rounded-lg hover:bg-gray transition-all duration-150">
+              <div className="bg-white flex justify-center items-center gap-2 py-2 w-fit px-3 border-2 border-transparent rounded-lg hover:bg-gray transition-all duration-150">
                 <Image src={arrowIcon} height={40} width={40} />
                 <p className="text-xl font-medium">VISIT</p>
               </div>
