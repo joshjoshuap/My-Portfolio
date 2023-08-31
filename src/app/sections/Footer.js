4947801c0c3a2d7bdc41f0515134cc3af985f3d7
Joshua <joshjoshuap1@gmail.com>
2023-08-31 18:45:18 +0800

    style: footer section responsive

--- src/app/sections/Footer.js
+++ src/app/sections/Footer.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Footer() {
   let currentYear = new Date().getFullYear();
   return (
-    <footer className="flex justify-center text-xl gap-1 p-5">
+    <footer className="flex flex-col justify-center gap-1 p-5 text-xl text-center md:flex-row">
       <p>&copy; Copyright {currentYear} | </p>
       <Link href="https://www.joshjoshuap.site/">Build by Joshua Pautanes</Link>
     </footer>
