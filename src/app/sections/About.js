c52de40e09060ff6502f62fb6056a47f070e35f1
Joshua <joshjoshuap1@gmail.com>
2023-08-31 17:35:02 +0800

    style: about responsive

--- src/app/sections/About.js
+++ src/app/sections/About.js
@@ -1,21 +1,23 @@
 export default function About() {
   return (
-    <div className="w-10/12 py-12 mx-auto" id="about">
-      <h1 className="text-6xl text-blue font-serif font-bold font mb-5">About</h1>
-      <p className="mb-5 text-xl indent-10 text-black">
+    <div className="w-11/12 py-12 mx-auto text-black xl:w-10/12" id="about">
+      <h1 className="mb-5 font-serif text-6xl font-bold text-center md:text-left text-blue font">
+        About
+      </h1>
+      <p className="mb-5 text-lg lg:text-xl lg:text-justify indent-3 lg:indent-10">
         I graduated with Bachelor's Degree in Information Technology at Quezon City University and
         Senior High in ICT strand at STI College Novaliches. Studied programming, web development,
         and software. Have skills and knowledge in web development, web design, and database
         management
       </p>
 
-      <p className="mb-5 text-xl indent-10">
+      <p className="mb-5 text-lg lg:text-xl lg:text-justify indent-3 lg:indent-10">
         I work in a computer store as a sales, technician and graphic designer. While working as a
         computer tech, in my spare time I upskill and improve my programming skill. I want to be
         good at programming also in graphic designing.
       </p>
 
-      <p className="mb-5 text-xl indent-10">
+      <p className="mb-5 text-lg lg:text-xl lg:text-justify indent-3 lg:indent-10">
         In Front-End I usually use ReactJS, NextJS, Bootstrap, and TailwindCSS. While in Back-End
         development I use NodeJS, ExpressJS, RestAPI, and MongoDB. Also familiar with other
         programming language such as PHP and C#.
