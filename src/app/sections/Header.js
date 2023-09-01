ff78698270022fc54a1f2e8130691239e40ffe35
Joshua <joshjoshuap1@gmail.com>
2023-09-01 15:08:11 +0800

    refractor: social media loop

--- src/app/sections/Header.js
+++ src/app/sections/Header.js
@@ -7,9 +7,16 @@ import instagramIcon from '/public/icons/instagram-icon.svg';
 import linkedinIcon from '/public/icons/linkedin-icon.svg';
 import githubIcon from '/public/icons/github-icon.svg';
 
+let socialMediaIcon = [
+  { alt: 'Facebook Icon', icon: facebookIcon, link: 'https://www.facebook.com/joshjoshuap1' },
+  { alt: 'Instagram Icon', icon: instagramIcon, link: 'https://www.instagram.com/joshjoshuap1/' },
+  { alt: 'Linkedin Icon', icon: linkedinIcon, link: 'https://www.linkedin.com/in/joshuapautanes/' },
+  { alt: 'Github Icon', icon: githubIcon, link: 'https://github.com/joshjoshuap' },
+];
+
 export default function Header() {
   return (
-    <header className="pt-8 pb-6 font-mono font-normal xl:pb-10 lg:pt-10 xl:pt-16 bg-blue">
+    <header className="pt-8 pb-6 font-mono xl:pb-10 lg:pt-10 xl:pt-16 bg-blue">
       <div className="w-11/12 mx-auto text-white md:grid md:grid-cols-2 xl:w-10/12 ">
         <div className="text-center md:text-left">
           <h3 className="pb-2 text-xl text-center md:text-left lg:text-2xl xl:text-4xl text-lightorange">
@@ -24,18 +31,11 @@ export default function Header() {
           </h3>
 
           <div className="flex justify-center gap-2 mt-4 md:justify-start lg:mt-6 xl:mt-10">
-            <Link href="https://www.facebook.com/joshjoshuap1" target="_blank">
-              <Image src={facebookIcon} className="w-14 lg:w-16 2xl:w-20" />
-            </Link>
-            <Link href="https://www.instagram.com/joshjoshuap1/" target="_blank">
-              <Image src={instagramIcon} className="w-14 lg:w-16 2xl:w-20" />
-            </Link>
-            <Link href="https://www.linkedin.com/in/joshuapautanes/" target="_blank">
-              <Image src={linkedinIcon} className="w-14 lg:w-16 2xl:w-20" />
-            </Link>
-            <Link href="https://github.com/joshjoshuap" target="_blank">
-              <Image src={githubIcon} className="w-14 lg:w-16 2xl:w-20" />
-            </Link>
+            {socialMediaIcon.map((item) => (
+              <Link href={item.link} target="_blank">
+                <Image src={item.icon} alt={item.alt} className="w-14 lg:w-16 2xl:w-20" />
+              </Link>
+            ))}
           </div>
           <Link href="#about">
             <Button className="px-8 py-6 mt-6 text-2xl text-white rounded-md xl:mt-10 bg-lightgreen">
