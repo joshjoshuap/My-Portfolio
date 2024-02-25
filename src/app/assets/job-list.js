9f2a53b63372c3922da0594f31d6f7e601b65156
Joshua <joshjoshuap1@gmail.com>
2024-02-25 15:55:56 +0800

    refractor: refractor job descriptions

---
+++ src/app/assets/job-list.js
@@ -0,0 +1,47 @@
+let jobExperienceList = [
+  {
+    positionTitle: 'PC Engineer',
+    companyDesc: 'Azeus Philippines System Limited Inc - October 2023 to Present',
+    jobDesc: [
+      {
+        jobDescItem:
+          'Provide assistance to employees tickets and technical concern as well as helping the company technical problems',
+      },
+      {
+        jobDescItem:
+          'Assisting the System Administrator in monitoring and maintaining the system and IT infrastructure',
+      },
+      {
+        jobDescItem:
+          'Build and troubleshoot desktop workstation, computer peripherals, networks and servers',
+      },
+      {
+        jobDescItem:
+          'Install and configure Server’s VM using KVM and VMware in different operating system such as Windows and Linux',
+      },
+      {
+        jobDescItem:
+          'Train and guide employees on using computer hardware, software programs and user accounts',
+      },
+    ],
+  },
+  {
+    positionTitle: 'Sales / Computer Technician',
+    companyDesc: 'Starworks Integrated Services Inc / OPNetworks Inc - Feb 2023 to July 2023',
+    jobDesc: [
+      {
+        jobDescItem:
+          'Build, repair, and troubleshoot client computer hardware, operating system and software programs',
+      },
+      {
+        jobDescItem:
+          'Selling and advertising computer products and accessories through store, social media, email and phone calls',
+      },
+      {
+        jobDescItem: 'Managing and monitoring store products inventory using excel',
+      },
+    ],
+  },
+];
+
+export default jobExperienceList;
--- src/app/sections/About.js
+++ src/app/sections/About.js
@@ -1,13 +1,31 @@
 import Section from '../components/Section';
+import jobExperienceList from '../assets/job-list';
 
 let Paragraph = ({ children }) => {
   return <p className="mb-2 text-justify text-md lg:text-lg indent-3 lg:indent-10">{children}</p>;
+};
+
+let JobContainer = ({ positionTitle, companyDesc, jobDesc }) => {
+  return (
+    <div className="my-5">
+      <p className="text-2xl font-semibold text-teal-800">{positionTitle}</p>
+      <p className="text-sm italic text-neutral-700">{companyDesc}</p>
+      <div className="w-10/12 mx-auto mt-3">
+        <ul className="text-sm list-disc">
+          {jobDesc.map((item) => {
+            return <li>{item.jobDescItem}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default function About() {
   return (
     <Section id="about" titleColor="text-lightblue" bgColor="bg-white">
       <div className="flex flex-col lg:flex-row lg:gap-10">
+        {/* MY STORY CARD */}
         <div className="mb-10 lg:w-1/2 lg:mb-0">
           <h1 className="mb-5 text-4xl font-medium text-center lg:mb-10 lg:text-6xl text-emerald-700">
             MY STORY
@@ -34,61 +52,19 @@ export default function About() {
             give technical assistance to employee and maintain the Infrasctucture of the company
           </Paragraph>
         </div>
+
+        {/* JOB EXPERIENCE */}
         <div className="lg:w-1/2">
           <h1 className="mb-5 text-4xl text-center text-red-600 lg:mb-10 lg:text-6xl">
             JOB EXPERIENCE
           </h1>
-
-          <div className="my-2">
-            <p className="text-2xl font-semibold text-teal-800">PC Engineer (IT Support)</p>
-            <p className="text-sm italic text-neutral-700">
-              Azeus Philippines System Limited Inc - October 2023 to Present
-            </p>
-            <div className="w-10/12 mx-auto mt-3">
-              <ul className="text-sm list-disc">
-                <li>
-                  Provide assistance to employees tickets and technical concern as well as helping
-                  the company technical problems
-                </li>
-                <li>
-                  Assisting the System Administrator in monitoring and maintaining the system and IT
-                  infrastructure
-                </li>
-                <li>
-                  Build and troubleshoot desktop workstation, computer peripherals, networks and
-                  servers
-                </li>
-                <li>
-                  Install and configure Server’s VM using KVM and VMware in different operating
-                  system such as Windows and Linux
-                </li>
-                <li>
-                  Train and guide employees on using computer hardware, software programs and user
-                  accounts
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="my-2">
-            <p className="text-2xl font-semibold text-teal-800">Sales / Computer Technician</p>
-            <p className="text-sm italic text-neutral-700">
-              Starworks Integrated Inc / OPNetworks Inc - Februay 2023 to July 2023
-            </p>
-            <div className="w-10/12 mx-auto mt-3">
-              <ul className="text-sm list-disc">
-                <li>
-                  Build, repair, and troubleshoot client computer hardware, operating system and
-                  software programs
-                </li>
-                <li>
-                  Selling and advertising computer products and accessories through store, social
-                  media, email and phone calls
-                </li>
-                <li>Managing and monitoring store products inventory using excel</li>
-              </ul>
-            </div>
-          </div>
+          {jobExperienceList.map((item) => (
+            <JobContainer
+              positionTitle={item.positionTitle}
+              companyDesc={item.companyDesc}
+              jobDesc={item.jobDesc}
+            />
+          ))}
         </div>
       </div>
     </Section>
