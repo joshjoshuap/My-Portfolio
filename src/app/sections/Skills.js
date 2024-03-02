99e5c5719549df006e9886f475ffb99a5778c065
Joshua <joshjoshuap1@gmail.com>
2024-03-02 14:41:44 +0800

    chore: skills section, change layout and design

--- src/app/sections/Skills.js
+++ src/app/sections/Skills.js
@@ -1,5 +1,5 @@
+import Image from 'next/image';
 import Section from '../components/Section';
-import SkillsCard from '../components/SkillsCard';
 import {
   htmlIcon,
   cssIcon,
@@ -138,9 +138,53 @@ export default function Skills() {
       titleColor="text-white"
       titleIsCenter="Yes"
       bgColor="bg-gradient-to-r from-slate-900 via-cyan-900 to-sky-900">
-      {skillList.map((item) => (
-        <SkillsCard SkillTitle={item.title} icons={item.skills} className={item.titleColor} />
-      ))}
+      <div className="grid grid-cols-2 gap-10 mt-20">
+        <div className="p-5 bg-white border-4 border-white rounded-sm">
+          <h1 className="text-4xl font-semibold text-center text-teal-800">DEVELOPMENT</h1>
+          <div className="grid grid-cols-5 mt-5">
+            <Image
+              shadow="sm"
+              className=""
+              height={100}
+              width={100}
+              alt={htmlIcon}
+              src={htmlIcon}
+            />
+            <Image shadow="sm" className="" height={100} width={100} src={cssIcon} />
+            <Image shadow="sm" className="" height={100} width={100} src={sassIcon} />
+            <Image shadow="sm" className="" height={100} width={100} src={bootstrapIcon} />
+            <Image shadow="sm" className="" height={100} width={100} src={tailwindIcon} />
+            <Image shadow="sm" className="" height={100} width={100} src={reactjsIcon} />
+            <Image shadow="sm" className="" height={100} width={100} src={nodejsIcon} />
+            <Image shadow="sm" className="" height={100} width={100} src={mongodbIcon} />
+            <Image shadow="sm" className="" height={100} width={100} src={gitIcon} />
+            <Image shadow="sm" className="" height={100} width={100} src={githubIcon} />
+          </div>
+        </div>
+        <div className="p-5 bg-white border-4 border-white rounded-sm">
+          <h1 className="text-4xl font-semibold text-center text-teal-800">PROGRAMMING</h1>
+          <div className="grid grid-cols-5 mt-5">
+            <Image shadow="sm" className="" height={100} width={100} src={javascriptIcon} />
+            <Image shadow="sm" className="" height={100} width={100} src={mysqlIcon} />
+            <Image shadow="sm" className="" height={100} width={100} src={bashIcon} />
+          </div>
+        </div>
+        <div className="p-5 bg-white border-4 border-white rounded-sm">
+          <h1 className="text-4xl font-semibold text-center text-teal-800">
+            SYSTEM ADMINISTRATION
+          </h1>
+          <div className="grid grid-cols-5 mt-5">
+            <Image shadow="sm" className="" height={100} width={100} src={ubuntuIcon} />
+          </div>
+        </div>
+        <div className="p-5 bg-white border-4 border-white rounded-sm">
+          <h1 className="text-4xl font-semibold text-center text-teal-800">SOFTWARE - TOOLS</h1>
+          <div className="grid grid-cols-5 mt-5">
+            <Image shadow="sm" className="" height={100} width={100} src={vscodeIcon} />
+            <Image shadow="sm" className="" height={100} width={100} src={visualstudioIcon} />
+          </div>
+        </div>
+      </div>
     </Section>
   );
 }
